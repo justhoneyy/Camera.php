d4a47d7b79bfc888bb5acc12325e121f23ae44e5
Honey <104066113+justhoneyy@users.noreply.github.com>
2025-08-08 22:42:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // Set the root folder where your index.html lives
   root: 'public',
-
   build: {
-    // Output built files to /dist
     outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-
-    // Optional: base path for serving
-    // base: './'
+    emptyOutDir: true
   },
-
   server: {
-    port: 5173, // Local dev port (optional)
-    open: true  // Auto-open in browser
+    port: 3000
   }
 });
